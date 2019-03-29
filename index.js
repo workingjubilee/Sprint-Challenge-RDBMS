94eea605eb41ac8e62362835cf58a364c71438cc
workingjubilee <workingjubilee@gmail.com>
2019-03-29 08:41:13 -0700

    Detail index with functionality

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const helmet = require('helmet');
+const cors = require('cors');
 
 const server = express();
+
+server.use(cors());
+server.use(helmet());
+server.use(express.json())
 
 
 server.listen(5000, () => {
